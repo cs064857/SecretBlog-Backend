57ebe5b59db17017bc783e99cdf838e2f120606e
Secret <cs064857@yahoo.com.tw>
2024-10-27 23:53:38 +0800

    2024/10/27

---
+++ secret-minio/src/main/java/com/shijiawei/secretblog/minio/service/MmsUploadService.java
@@ -0,0 +1,10 @@
+package com.shijiawei.secretblog.minio.service;
+
+/**
+ * ClassName: MmsUploadService
+ * Description:
+ *
+ * @Create 2024/10/27 下午11:51
+ */
+public interface MmsUploadService {
+}
