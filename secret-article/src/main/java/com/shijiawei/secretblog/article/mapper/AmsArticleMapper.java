00b69bd3ef6a9d9670683a5e3072ed37d3053ce7
Secret <cs064857@yahoo.com.tw>
2025-09-25 01:31:47 +0800

    feat: refactor AmsArticleMapper and XML for improved data mapping and query efficiency

--- secret-article/src/main/java/com/shijiawei/secretblog/article/mapper/AmsArticleMapper.java
+++ secret-article/src/main/java/com/shijiawei/secretblog/article/mapper/AmsArticleMapper.java
@@ -4,22 +4,16 @@ import com.shijiawei.secretblog.article.entity.AmsArticle;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.shijiawei.secretblog.article.vo.AmsArticleTagsVo;
 import com.shijiawei.secretblog.article.vo.AmsArticleVo;
-import io.lettuce.core.dynamic.annotation.Param;
-import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 /**
-* @author User
-* @description 针对表【ams_article(文章內容)】的数据库操作Mapper
-* @createDate 2024-08-26 00:17:06
-* @Entity com.shijiawei.secretblog.article.entity.AmsArticle
-*/
-
+ * 針對表 ams_article(文章內容) 的數據庫操作 Mapper
+ *
+ * @author User
+ * @since 2024-08-26
+ */
 public interface AmsArticleMapper extends BaseMapper<AmsArticle> {
 
     AmsArticleTagsVo getArticleTagVoList(@Param("articleId") Long articleId);
     AmsArticleVo getArticleVo(@Param("articleId") Long articleId);
 }
-
-
-
-
