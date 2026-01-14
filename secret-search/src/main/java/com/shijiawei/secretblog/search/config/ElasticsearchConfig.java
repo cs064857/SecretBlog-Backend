2db0001b8bbca444cc707cfaabb9a3b8c4cefed4
Secret <cs064857@yahoo.com.tw>
2026-01-15 02:01:23 +0800

    feat: 更新Elasticsearch配置以支持從環境變數讀取連線參數

--- secret-search/src/main/java/com/shijiawei/secretblog/search/config/ElasticsearchConfig.java
+++ secret-search/src/main/java/com/shijiawei/secretblog/search/config/ElasticsearchConfig.java
@@ -5,20 +5,25 @@ import co.elastic.clients.json.jackson.JacksonJsonpMapper;
 import co.elastic.clients.transport.rest_client.RestClientTransport;
 import org.apache.http.HttpHost;
 import org.elasticsearch.client.RestClient;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ElasticsearchConfig {
 
+    //從配置檔案讀取Elasticsearch連線地址
+    @Value("${spring.elasticsearch.uris}")
+    private String uris;
+
     @Bean
     public ElasticsearchClient elasticsearchClient() {
-        var httpHost = new HttpHost("localhost", 9200);
+        //使用配置的URI動態建立HttpHost
+        HttpHost httpHost = HttpHost.create(uris);
 
         RestClient build = RestClient.builder(httpHost).build();
 
         var transport = new RestClientTransport(build, new JacksonJsonpMapper());
-
 
         return new ElasticsearchClient(transport);
     }
